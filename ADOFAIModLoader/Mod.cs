025079c2378fcfea1e34fe41e43fa1d798baebfe
bluecard <sideofsidegd@gmail.com>
2022-02-20 23:14:13 +0900

    초기 커밋.

---
+++ ADOFAIModLoader/Mod.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace ADOFAIModLoader
+{
+    public class Mod
+    {
+        public virtual void Startup()
+        {
+
+        }
+        public virtual string GetModName()
+        {
+            return "Unknown";
+        }
+    }
+    public class ModLogger
+    {
+        public ModLogger(Type MainClass)
+        {
+            mainclass = MainClass;
+        }
+        private Type mainclass;
+
+        public void Log(object msg)
+        {
+            var msg1 = "UNKNOWN";
+            if (mainclass.BaseType.Equals(typeof(Mod)))
+            {
+                var i = ModLoaderMain.mods[mainclass];
+                 msg1 = "[" + i.GetModName() + "] " + msg;
+            }
+            if (mainclass.BaseType.Equals(typeof(Plugin)))
+            {
+                var i = ModLoaderMain.plugins[mainclass];
+                msg1 = "[" + i.GetPluginName() + "] " + msg;
+            }
+            Debug.Log(msg1);
+            
+        }
+
+        internal static void InternalLog(object msg)
+        {
+            var msg1 = "[AML] " + msg;
+            Debug.Log(msg1);
+        }
+    }
+}
---
+++ ADOFAIModLoader/ModLoaderMain.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+using UnityEngine;
+using System.Threading;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+namespace ADOFAIModLoader
+{
+    public class ADOBASECLASS : ADOBase
+    {
+       
+    }
+    public static class ModLoaderMain
+    {
+        public static Dictionary<Type, Mod> mods = new Dictionary<Type, Mod>();
+        public static Dictionary<Type, Plugin> plugins = new Dictionary<Type, Plugin>();
+
+        public static void LoadMods()
+        {
+            var stopwatch = Stopwatch.StartNew();
+           
+            var pluginDir = new DirectoryInfo("./Plugins");
+            var modDir = new DirectoryInfo("./Mods");
+            if (!pluginDir.Exists)
+            {
+                pluginDir.Create();
+            }
+            if (!modDir.Exists)
+            {
+                modDir.Create();
+            }
+            foreach (DirectoryInfo d in pluginDir.GetDirectories())
+            {
+                DirectoryInfo libfolder = new DirectoryInfo(d.FullName + "Libs");
+                if (libfolder.Exists)
+                {
+                    foreach (FileInfo f in libfolder.GetFiles("*.dll"))
+                    {
+                        Assembly.LoadFile(f.FullName);
+                    }
+                }
+                var fa = d.GetFiles("*.dll")[0];
+                var asm = Assembly.LoadFile(fa.FullName);
+                foreach (Type t in asm.GetTypes())
+                {
+                    if (t.BaseType.Equals(typeof(Plugin)))
+                    {
+                        var i = (Plugin)Activator.CreateInstance(t);
+                        var pluginName = i.GetPluginName();
+                        plugins.Add(t, i);
+                        i.Startup();
+                        ModLogger.InternalLog("Loaded Plugin " + pluginName);
+                    }
+                }
+            }
+            foreach (DirectoryInfo d in modDir.GetDirectories())
+            {
+                DirectoryInfo libfolder = new DirectoryInfo(d.FullName + "Libs");
+                if (libfolder.Exists)
+                {
+                    foreach (FileInfo f in libfolder.GetFiles("*.dll"))
+                    {
+                        Assembly.LoadFile(f.FullName);
+                    }
+                }
+
+                var fa = d.GetFiles("*.dll")[0];
+                var asm = Assembly.LoadFile(fa.FullName);
+                foreach (Type t in asm.GetTypes())
+                {
+                    if (t.BaseType.Equals(typeof(Mod)))
+                    {
+                        var i = (Mod)Activator.CreateInstance(t);
+                        var modName = i.GetModName();
+                        mods.Add(t, i);
+                        i.Startup();
+                        ModLogger.InternalLog("Loaded Mod " + modName);
+                    }
+                    
+                }
+                stopwatch.Stop();
+                ModLogger.InternalLog("Loaded " + mods.Count + " mods and " + plugins.Count + " plugins in " + stopwatch.ElapsedMilliseconds + "ms.");
+            }
+        }
+
+    }
+}
